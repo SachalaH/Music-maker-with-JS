5bb3f074198891d1b2e0fef300d4540f54da4b37
SachalaH <59394760+SachalaH@users.noreply.github.com>
2020-04-23 12:30:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll('.sound')
+    const pads = document.querySelectorAll('.pads div')
+    const visuals = document.querySelector('.visuals')
+    colors = [
+        '#f86067',
+        '#7ee4b1',
+        '#e76ff6',
+        '#f8a354',
+        '#f6f86d',
+        '#9b9af8'
+    ];
+
+    // Playing the audio with pads
+
+    pads.forEach((pad, index) =>{
+        pad.addEventListener('click', function(){
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+            createAnimation(index);
+        })
+    })
+
+    // Handling the animations
+
+    const createAnimation = (index) => {
+        const ball = document.createElement('div');
+        visuals.appendChild(ball);
+        ball.style.backgroundColor = colors[index];
+        ball.addEventListener('animationend', ()=>{
+            visuals.removeChild(ball)
+        });
+        if(index > 2){
+            ball.style.animation = "rev-jump 1s ease";
+        }
+        else{
+            ball.style.animation = 'jump 1s ease';
+        }
+        
+        
+        
+    }
+});
